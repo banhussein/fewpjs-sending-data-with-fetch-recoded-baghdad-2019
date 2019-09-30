430a1ca20418122245840f079f2f441c72b07e00
ban hussein <banhussien97@gmail.com>
2019-09-30 16:50:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your code here
+function submitData(name,email){
+  const requestOption={
+    headers: {
+      'content-Type':'application/json'
+      'Accept':'application/json'
+    },
+    method:"POST",
+    body:JSON.stringfy({name:name,email:email})
+    
+  })
+}
